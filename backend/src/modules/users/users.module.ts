bd682f7b2b21fa9c3429257e5cca12c61cab910e
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 20:47:16 -0300

    importando workareas e cloudinary

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -4,10 +4,14 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserRepository } from './users.repository';
 import { UsersController } from './users.controller';
+import { WorkAreasModule } from '../workAreas/workAreas.module';
+import { cloudinaryModule } from 'src/providers/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Users.name, schema: UserSchema }]),
+    WorkAreasModule,
+    cloudinaryModule,
   ],
   controllers: [UsersController],
   providers: [UsersService, UserRepository],
