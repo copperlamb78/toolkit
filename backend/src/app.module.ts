e536698f3c212f51fc4815fc6eece08b8bf97d68
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 17:08:35 -0300

    chore(app): Adicionando modulos de todas as features novas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { WorkAreasModule } from './modules/workAreas/workAreas.module';
+import { CategoryModule } from './modules/category/category.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_LINK'),
+      }),
+    }),
+    UsersModule,
+    WorkAreasModule,
+    CategoryModule,
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
