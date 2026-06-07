cfc08c02adefef949fae6f93d483794f44780ed4
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 20:43:46 -0300

    feat(users): controller para updateUser

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import {
   ApiTags,
   ApiOperation,
@@ -6,7 +15,7 @@ import {
   ApiBadRequestResponse,
 } from '@nestjs/swagger';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './users.dto';
+import { CreateUserDto, UpdateUserDto } from './users.dto';
 
 @ApiTags('Users')
 @Controller('users')
@@ -28,7 +37,7 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @Post()
+  @Get()
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Listar usuários' })
   @ApiResponse({
@@ -37,5 +46,19 @@ export class UsersController {
   })
   async findAll() {
     return this.usersService.findAll();
+  }
+
+  @Patch('update/:id')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Atualizar usuário' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Usuário atualizado com sucesso.',
+  })
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.updateUser(id, updateUserDto);
   }
 }
