559bd47640260db0579dbdb8784132ed68126c2a
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 17:10:32 -0300

    feat: projects service

---
+++ backend/src/modules/projects/projects.service.ts
@@ -0,0 +1,61 @@
+import { cloudinaryHelper } from 'src/providers/cloudinary/cloudinary.helper';
+import { CreateProjectsDto } from './projects.dto';
+import { projectsRepository } from './projects.repository';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { CategoryService } from '../category/category.service';
+
+@Injectable()
+export class ProjectsService {
+  constructor(
+    private readonly projectsRepository: projectsRepository,
+    private readonly cloudinaryHelper: cloudinaryHelper,
+    private readonly categoryService: CategoryService,
+  ) {}
+
+  async create(data: CreateProjectsDto, files: any[]) {
+    const existingProjects = await this.projectsRepository
+      .findAll()
+      .then((projects) =>
+        projects.find((projects) => {
+          return projects.name === data.name;
+        }),
+      );
+
+    if (existingProjects) {
+      throw new BadRequestException('Já existe um projeto com esse nome');
+    }
+
+    const photos = await this.cloudinaryHelper.uploadFiles(
+      files.map((photo: any) => ({
+        buffer: photo.buffer,
+        originalname: photo.originalname,
+        mimetype: photo.mimetype,
+      })),
+      'projects',
+    );
+
+    const photosUrls = photos.map((photo: any) => photo.url);
+
+    return this.projectsRepository.create({
+      ...data,
+      photosList: photosUrls,
+    });
+  }
+
+  async findAll() {
+    return this.projectsRepository.findAll();
+  }
+
+  async findByCategory(category: string) {
+    const categories = await this.categoryService.findAll();
+    const existingCategory = categories.find((dbCategory) => {
+      return dbCategory.name === category;
+    });
+
+    if (!existingCategory) {
+      throw new BadRequestException('Categoria não encontrada');
+    }
+
+    return this.projectsRepository.findByCategory(category);
+  }
+}
