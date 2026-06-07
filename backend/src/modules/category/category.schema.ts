7ffb27b0ea54ef3f3bbe9f7e51cbe2bf31528994
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 17:09:39 -0300

    feat: category schema

---
+++ backend/src/modules/category/category.schema.ts
@@ -0,0 +1,8 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Category {
+  @Prop({ required: true })
+  name!: string;
+}
+export const CategorySchema = SchemaFactory.createForClass(Category);
