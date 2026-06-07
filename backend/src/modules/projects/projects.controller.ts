4383eccff24112d029c38dbc9e3ee7bab441be76
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 17:09:52 -0300

    feat: projects controller

---
+++ backend/src/modules/projects/projects.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ApiConsumes } from '@nestjs/swagger';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
+import { ProjectsService } from './projects.service';
+import { CreateProjectsControllerDto } from './projects.dto';
+
+@ApiTags('Projects')
+@Controller('projects')
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Post()
+  @UseInterceptors(FilesInterceptor('PhotosList'))
+  @ApiConsumes('multipart/form-data')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Cadastro de Projeto' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Projeto criado com sucesso.',
+  })
+  @ApiBadRequestResponse({
+    description:
+      'Dados de entrada inválidos ou Projeto já existe (por exemplo, nome duplicado).',
+  })
+  async create(
+    @UploadedFiles() files: any[],
+    @Body() createProjectsControllerDto: CreateProjectsControllerDto,
+  ) {
+    return this.projectsService.create(createProjectsControllerDto, files);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Listar Projetos' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Lista de projetos retornada com sucesso.',
+  })
+  async findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Get('/category/:category')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Listar Projetos por categoria' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Lista de projetos por categoria retornada com sucesso.',
+  })
+  async findByCategory(@Param('category') category: string) {
+    return this.projectsService.findByCategory(category);
+  }
+}
