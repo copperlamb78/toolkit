dc87dfad5ac2022fb8eb415ea3afcf555c62ecf3
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 17:08:54 -0300

    feat(main): Adicionando swagger para documentação

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação da API do ToolKit')
+    .setDescription('API para gerenciamento do ToolKit')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3250);
 }
 bootstrap();
