66ce212d4b6c8d15beee065ba3a8c33fe269a0ae
copperlamb78 <nascimento.gabriel2004@gmail.com>
2026-06-07 17:10:05 -0300

    feat: projects modules

---
+++ backend/src/modules/projects/projects.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ProjectsService } from './projects.service';
+import { projectsRepository } from './projects.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Projects, ProjectsSchema } from './projects.schema';
+import { cloudinaryModule } from 'src/providers/cloudinary/cloudinary.module';
+import { ProjectsController } from './projects.controller';
+import { CategoryModule } from '../category/category.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Projects.name, schema: ProjectsSchema },
+    ]),
+    cloudinaryModule,
+    CategoryModule,
+  ],
+  controllers: [ProjectsController],
+  providers: [ProjectsService, projectsRepository],
+  exports: [ProjectsService],
+})
+export class ProjectsModule {}
